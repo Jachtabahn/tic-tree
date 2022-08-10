2949ad0e0b9b0742bf54428ac4982dd27a9fe33f
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2022-08-10 18:18:36 +0700

    how to create an image from a Tic state

--- visualize.py
+++ visualize.py
@@ -1,57 +1,206 @@
-import numpy as np
-
-from bokeh.io import curdoc, show, export_svg
-from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Rect
-from bokeh.layouts import row
-from bokeh.colors import RGB
 from bokeh.models.annotations import Label
-
+from info import info
+import bokeh.io
+import bokeh.models
+import numpy
 
 state = {
-  "board": [
-    [-1, 1, -1],
-    [0, -1, 1],
-    [0, 1, 0]
+  'board': [
+    [-1, -1, 1],
+    [-1, -1, 1],
+    [0, 0, 0]
   ],
-  "current_player": "O",
-  "tried_actions": [
-    [1, 0],
-    [2, 2]
+  'current_player': 'O',
+  'tried_actions': [
+    [2, 0],
+    [2, 1],
+    [2, 2],
   ],
-  "untried_actions": [
-    [2, 0]
+  'untried_actions': [
   ],
-  "best_action": [1, 0],
-  "best_outcome": -0.2
+  'best_action': [2, 2],
+  'best_outcome': 1
 }
 
-x = [1, 2, 1.5, 1.5]
-y = [1.5, 1.5, 2, 1]
-w = [0.2, 0.2, 3, 3]
-h = [3, 3, 0.2, 0.2]
+# PYTHON HACK: Convert a dict object into a an object where the dict keys are attributes
+# See: https://stackoverflow.com/questions/59250557/how-to-convert-a-python-dict-to-a-class-object
+# =====================================================================
+class ObjectFromDict:
+  def __init__(self, dictionary):
+    for key, value in dictionary.items():
+      setattr(self, key, value)
+state = ObjectFromDict(state)
+# =====================================================================
 
-source = ColumnDataSource(dict(x=x, y=y, w=w, h=h))
+assert state.best_action in state.tried_actions
 
-plot = Plot(
-    title=None, width=300, height=300,
-    min_border=0, toolbar_location=None, output_backend="svg")
+plot = bokeh.models.Plot(
+  width = 300,
+  height = 300,
+  output_backend = "svg",
+  min_border = 0,
+  toolbar_location = None,
+  background_fill_alpha = 0,
+  outline_line_alpha = 0
+)
 
-glyph = Rect(x="x", y="y", width="w", height="h", fill_color="#cab2d6")
+# Orangene Felder: Die noch nicht ausprobierten Züge
+# =====================================================================
+source = bokeh.models.ColumnDataSource({
+  'x': [action[1] + 0.5 for action in state.untried_actions],
+  'y': [2 - action[0] + 0.5 for action in state.untried_actions],
+  'w': [1] * len(state.untried_actions),
+  'h': [1] * len(state.untried_actions)
+})
+glyph = bokeh.models.Rect(
+  x = "x",
+  y = "y",
+  width = "w",
+  height = "h",
+  line_alpha = 0,
+  fill_color = "#ff8129"
+)
+plot.add_glyph(source, glyph)
+# =====================================================================
+
+# Graue Felder: Die bereits ausprobierten Züge
+# =====================================================================
+source = bokeh.models.ColumnDataSource({
+  'x': [action[1] + 0.5 for action in state.tried_actions],
+  'y': [2 - action[0] + 0.5 for action in state.tried_actions],
+  'w': [1] * len(state.tried_actions),
+  'h': [1] * len(state.tried_actions)
+})
+glyph = bokeh.models.Rect(
+  x = "x",
+  y = "y",
+  width = "w",
+  height = "h",
+  line_alpha = 0,
+  fill_color = "#9e928a"
+)
+plot.add_glyph(source, glyph)
+# =====================================================================
+
+# Grünes Feld: Der beste Zug
+# =====================================================================
+source = bokeh.models.ColumnDataSource({
+  'x': [state.best_action[1] + 0.5],
+  'y': [2 - state.best_action[0] + 0.5],
+  'w': [1],
+  'h': [1]
+})
+glyph = bokeh.models.Rect(
+  x = "x",
+  y = "y",
+  width = "w",
+  height = "h",
+  line_alpha = 0,
+  fill_color = "#48f000"
+)
 plot.add_glyph(source, glyph)
 
-mytext = Label(x=0, y=-0.1, text='X', text_font_size='50pt')
-plot.add_layout(mytext)
+# Grauer Teil: Das beste Resultat
+source = bokeh.models.ColumnDataSource({
+  'x': [state.best_action[1] + 0.5],
+  'y': [2 - state.best_action[0] + 0.5],
+  'w': [0.4],
+  'h': [0.1]
+})
+glyph = bokeh.models.Rect(
+  x = "x",
+  y = "y",
+  width = "w",
+  height = "h",
+  line_alpha = 0,
+  fill_color = "#9e928a"
+)
+plot.add_glyph(source, glyph)
 
-mytext = Label(x=1.1, y=1, text='O', text_font_size='50pt')
-plot.add_layout(mytext)
+# Dreieck: Das beste Resultat
+if state.best_outcome == 1:
+  source = bokeh.models.ColumnDataSource({
+    'x': [state.best_action[1] + 0.5],
+    'y': [2 - state.best_action[0] + 0.5 + 0.15],
+  })
+  triangle = bokeh.models.Scatter(
+    x = "x",
+    y = "y",
+    line_alpha = 0,
+    fill_color = "#115ab2",
+    size = 25,
+    angle = 0,
+    marker = 'triangle'
+  )
+  plot.add_glyph(source, triangle)
 
-mytext = Label(x=0, y=2, text='X', text_font_size='50pt')
-plot.add_layout(mytext)
+if state.best_outcome == -1:
+  source = bokeh.models.ColumnDataSource({
+    'x': [state.best_action[1] + 0.5],
+    'y': [2 - state.best_action[0] + 0.5 - 0.15],
+  })
+  triangle = bokeh.models.Scatter(
+    x = "x",
+    y = "y",
+    line_alpha = 0,
+    fill_color = "#b21111",
+    size = 25,
+    angle = numpy.pi,
+    marker = 'triangle'
+  )
+  plot.add_glyph(source, triangle)
+# =====================================================================
 
-plot.axis.visible = False
-plot.grid.visible = False
-plot.background_fill_alpha = 0
+# Das Gitter des Bretts
+# =====================================================================
+source = bokeh.models.ColumnDataSource({
+  'x': [1, 2, 1.5, 1.5],
+  'y': [1.5, 1.5, 2, 1],
+  'w': [0.2, 0.2, 3, 3],
+  'h': [3, 3, 0.2, 0.2]
+})
+glyph = bokeh.models.Rect(
+  x = "x",
+  y = "y",
+  width = "w",
+  height = "h",
+  fill_color="#000000"
+)
+plot.add_glyph(source, glyph)
+# =====================================================================
 
-export_svg(plot, filename="tic_state.svg")
+# Die bereits gemachten Züge auf dem Spielbrett: Der Spielzustand
+# =====================================================================
+for row_index, row in enumerate(state.board):
+  for column_index, token in enumerate(row):
+    if token in [1, -1]:
+      draw_symbol = None
+      if token == -1 and state.current_player == 'X':
+        draw_symbol = 'O'
+      if token == 1 and state.current_player == 'X':
+        draw_symbol = 'X'
+      if token == -1 and state.current_player == 'O':
+        draw_symbol = 'X'
+      if token == 1 and state.current_player == 'O':
+        draw_symbol = 'O'
+      assert draw_symbol is not None
 
-# manipulate SVG by removing the first <path> object
+      color = None
+      if token == -1:
+        color = '#b21111'
+      if token == 1:
+        color = '#115ab2'
+
+      token_text = Label(
+        x = column_index + 0.1,
+        y = 2 - row_index - 0.05 * row_index,
+        text = draw_symbol,
+        text_font_size = '50pt',
+        text_color = color
+      )
+      plot.add_layout(token_text)
+# =====================================================================
+
+
+
+bokeh.io.export_svg(plot, filename="tic_state.svg")
