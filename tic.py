618de4a15ca25f88edef4f698dbb66470bab0a40
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2022-08-17 16:03:54 +0700

    Mache Tooltips für jede Kante, wo steht, wer gewinnen wird bei diesem Zug.

--- tic.py
+++ tic.py
@@ -1,9 +1,10 @@
 from info import info
-import os
-import visualize
-import json
 import hashlib
+import json
 import numpy
+import os
+import time
+import visualize
 
 def is_among_list(special, arrays):
   for array in arrays:
@@ -21,23 +22,21 @@ side_diagonal = [(r, 2-r) for r in range(3)]
 
 possible_winning_streaks = possible_winning_rows + possible_winning_columns + [main_diagonal, side_diagonal]
 
-info(possible_winning_streaks)
-
 class State:
 
   def __init__(self, board):
     self.board = board
     self.current_player = self.get_current_player()
     self.winning_streak, self.immediate_result = self.determine_winning_streak()
-    state_text = str(self.board) + str(self.current_player)
-    self.hash = int(hashlib.sha256(state_text.encode('utf-8')).hexdigest(), 16)
+    # What comes out of .hexdigest() is of type str.
+    self.hash = int(hashlib.sha256(str(self.board).encode('utf-8')).hexdigest(), 16)
 
   def determine_winning_streak(self):
     for possible_winning_streak in possible_winning_streaks:
-      sum_of_token_numbers = sum(self.board[position] for position in possible_winning_streak)
-      if sum_of_token_numbers == 3:
+      sum_of_possibly_winning_token_numbers = sum(self.board[position] for position in possible_winning_streak)
+      if sum_of_possibly_winning_token_numbers == 3:
         return possible_winning_streak, X
-      if sum_of_token_numbers == -3:
+      if sum_of_possibly_winning_token_numbers == -3:
         return possible_winning_streak, O
     return None, None
 
@@ -46,8 +45,8 @@ class State:
     if token == O: return 'O'
 
   def get_current_player(self):
-    sum_of_token_numbers = self.board.sum()
-    if sum_of_token_numbers > 0:
+    sum_of_all_token_numbers = self.board.sum()
+    if sum_of_all_token_numbers > 0:
       return O
     else:
       return X
@@ -63,8 +62,7 @@ class State:
     if self.winning_streak is not None:
       return []
 
-    empty_positions = list(zip(*(numpy.array(self.board) == 0).nonzero()))
-    # empty_positions = list(zip(*(self.board == 0).nonzero()))
+    empty_positions = list(zip(*(self.board == 0).nonzero()))
 
     distinct_boards = []
     non_reducable_actions = []
@@ -119,16 +117,38 @@ class Node:
     tried_actions = [action for action, child_node in self.children.items() if child_node is not None]
     return tried_actions
 
-  def create_node(board, parent=None, last_action=None):
-
-    state = State(board)
+  def create_root_node():
+    state = State(board = numpy.array([
+      [0, 0, 0],
+      [0, 0, O],
+      [0, X, X]
+    ]))
 
     # Try to fetch a node from memory.
     if state.hash in node_from_state_hash:
       return node_from_state_hash[state.hash]
 
     # Fetching didn't work... create a node,
-    node = Node(state, parent, last_action)
+    node = Node(state, parent=None, last_action=None)
+
+    # then save it into memory,
+    node_from_state_hash[state.hash] = node
+
+    # and make it available for use.
+    return node
+
+  def create_child_node(self, last_action):
+
+    next_board = self.state.board.copy()
+    next_board[last_action] = self.state.current_player
+    state = State(next_board)
+
+    # Try to fetch a node from memory.
+    if state.hash in node_from_state_hash:
+      return node_from_state_hash[state.hash]
+
+    # Fetching didn't work... create a node,
+    node = Node(state, self, last_action)
 
     # then save it into memory,
     node_from_state_hash[state.hash] = node
@@ -138,14 +158,13 @@ class Node:
 
   def expand(self):
     for action in self.children:
-      next_board = self.state.board.copy()
-      next_board[action] = self.state.current_player
-      child_node = Node.create_node(next_board, self, action)
+      child_node = self.create_child_node(action)
       self.children[action] = child_node
 
       global how_many
-      if how_many < 100:
+      if how_many < 10000000000000000000:
         child_node.expand()
+        print(f'Computed: {how_many} Tic Tac Toe game tree nodes.')
         how_many += 1
 
         if child_node.children:
@@ -216,18 +235,15 @@ class Node:
     return level
 
   def __repr__(self):
-    return str({
-      'state': {
-        'board': self.state.board,
-        'current_player': self.state.current_player,
-        'hash': self.state.hash
-      },
-      'best_action': self.best_action,
-      'best_future_result': self.best_future_result,
-      # 'children': self.children
-    })
+    representation = ''
+    representation += f'state.board: {self.state.board}\n'
+    representation += f'state.current_player: {self.state.current_player}\n'
+    representation += f'state.hash: {self.state.hash}\n'
+    representation += f'best_action: {self.best_action}\n'
+    representation += f'best_future_result: {self.best_future_result}\n'
+    return representation
 
-  def get_children_hashes_and_best_action_indicators(self):
+  def get_children_hashes_and_actions(self):
     children_hashes = []
     for action, child_node in self.children.items():
       if child_node is not None:
@@ -242,55 +258,84 @@ class Node:
     vis_js_json = {}
 
     vis_js_json['nodes'] = []
+    visualized_how_many = 0
     for state_hash, node in node_from_state_hash.items():
+      visualized_how_many += 1
       state_level = node.determine_level()
-      info(state_level)
 
       # This vis.js configuration applies to this specific node.
       vis_node_declaration = {
         'id': state_hash,
         'level': state_level,
         'image': f'nodes/{node.state.hash}.svg',
-        'title': str(self)
+        'title': str(node)
       }
       vis_js_json['nodes'].append(vis_node_declaration)
 
       visualize.create_svg(node)
+      print(f'Visualized: {visualized_how_many} Tic Tac Toe game tree nodes.')
 
     vis_js_json['edges'] = []
     for state_hash, node in node_from_state_hash.items():
-      children_hashes = node.get_children_hashes_and_best_action_indicators()
-      info(state_hash)
-      info(children_hashes)
+      children_hashes = node.get_children_hashes_and_actions()
       for child_hash, action in children_hashes:
+
+        edge_color = '#000000'
+
+        # X gewinnt über diesen Zug.
+        if node.best_future_result == X and node.best_action == action:
+          edge_color = '#115ab2'
+          note = 'X will win'
+
+        # O gewinnt über diesen Zug.
+        if node.best_future_result == O and node.best_action == action:
+          edge_color = '#b21111'
+          note = 'O will win'
+
+        # Keiner gewinnt über diesen Zug.
+        if node.best_future_result == 0 and node.best_action == action:
+          edge_color = '#9e928a'
+          note = 'There will be draw'
+
+        next_board = node.state.board.copy()
+        next_board[action] = node.state.current_player
+        next_state = State(next_board)
+        # Try to fetch a node from memory.
+        assert next_state.hash in node_from_state_hash
+        next_node = node_from_state_hash[next_state.hash]
+
+        note = None
+        if next_node.best_future_result == X:
+          note = 'X will win'
+        if next_node.best_future_result == O:
+          note = 'O will win'
+        if next_node.best_future_result == 0:
+          note = 'There will be a draw'
+
+        if next_node.state.immediate_result == X:
+          note = 'X will win'
+        if next_node.state.immediate_result == O:
+          note = 'O will win'
+        if next_node.state.immediate_result is None and len(next_node.children) == 0:
+          note = 'There will be a draw'
 
         # This vis.js configuration applies to this specific edge.
         vis_edge_declaration = {
           'from': state_hash,
           'to': child_hash,
-          'color': '#008f0b' if self.best_action == action else '#000000',
-          'title': f'htmlTitle("<b>{action}</b>")'
+          'color': edge_color,
+          'title': f'htmlTitle("<b>{action}</b>: {note}")'
         }
         vis_js_json['edges'].append(vis_edge_declaration)
 
     return vis_js_json
 
+
+start_time = time.time()
 node_from_state_hash = {}
-
-node = Node.create_node(
-  board = numpy.array([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-  ])
-)
-
-node.expand()
-
-info(node)
-
-vis_js_json = node.create_vis_js_json()
-info(vis_js_json)
+root = Node.create_root_node()
+root.expand()
+vis_js_json = root.create_vis_js_json()
 with open('tree_info.js', 'w') as file:
   print('tree_info=', end = '', file=file)
   javascript_code = json.dumps(vis_js_json, indent = 2)
@@ -302,3 +347,6 @@ with open('tree_info.js', 'w') as file:
   javascript_code = javascript_code.replace('\\"', '"')
 
   print(javascript_code, end = '', file=file)
+
+print('Computed: Tic Tac Toe game tree.')
+print(f'Done in {time.time() - start_time} s.')
